ea3346fd23a57f1fb5136580d8052fd6bc77d493
eliusmgani <eliasmgani@gmail.com>
2022-01-26 19:16:56 +0000

    feat: patch to update deartment hsu to old record of lrpt childs

---
+++ hms_tz/patches/update_department_hsu_into_child_table.py
@@ -0,0 +1,43 @@
+from re import template
+import frappe
+from frappe.utils import nowdate
+
+
+def execute():
+    today = nowdate()
+    child_fields = [
+        {
+            "doctype": "Lab Test Template",
+            "field": "lab_test_prescription",
+            "item": "lab_test_code"
+        },
+        {
+            "doctype": "Radiology Examination Template",
+            "field": "radiology_procedure_prescription",
+            "item": "radiology_examination_template"
+        },
+        {
+            "doctype": "clinical Procedure Template",
+            "field": "procedure_prescription",
+            "item": "procedure"
+        },
+        {
+            "doctype": "Therapy Type",
+            "field": "therapies",
+            "item": "therapy_type"
+        },
+    ]
+
+    patient_encounters = frappe.get_all("Patient Encounter", {"encounter_date": ["<=", today]}, ["name"])
+    for encounter in patient_encounters:
+        encounter_doc = frappe.get_doc("Patient Encounter", encounter.name)
+        
+        for child in child_fields:
+            if encounter_doc.get(child.get("field")):
+                for row in encounter_doc.get(child.get("field")):
+                    if row.get(child.get("item")) and not row.department_hsu:
+                        template = frappe.get_doc(child.get("doctype"), row.get(child.get("item")))
+                        for option in template.company_options:
+                            if encounter_doc.company == option.company:
+                                row.department_hsu = option.service_unit
+                                row.db_update()
