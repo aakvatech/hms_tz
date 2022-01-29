312c90ff909206a6a6b2d4930a467781366f475a
eliusmgani <eliasmgani@gmail.com>
2022-01-29 13:17:04 +0000

    feat: frappe db escape

--- hms_tz/patches/update_department_hsu_into_child_table.py
+++ hms_tz/patches/update_department_hsu_into_child_table.py
@@ -5,43 +5,46 @@ from frappe.utils import nowdate
 def execute():
     today = nowdate()
     encounters = frappe.get_all("Patient Encounter", {"encounter_date": ["<=", today]}, ["name"], pluck="name")
+ 
+    frappe.db.sql("""
+        UPDATE `tabLab Prescription` lrpt
+        INNER JOIN `tabPatient Encounter` pe ON lrpt.parent = pe.name
+        INNER JOIN `tabLab Test Template` template ON lrpt.lab_test_code = template.name AND template.disabled = 0
+        INNER JOIN `tabHealthcare Company Option` hco ON template.name = hco.parent AND hco.company = pe.company
+        SET lrpt.department_hsu = hco.service_unit
+        WHERE pe.name IN (%s)
+    """%frappe.db.escape(tuple(encounters)))
+
 
     frappe.db.sql("""
-        UPDATE `tabLab Prescription` l
-        INNER JOIN `tabLab Test Template` lab ON l.lab_test_code = lab.name AND lab.disabled = 0
-        INNER JOIN `tabHealthcare Company Option` lab_hco ON lab.name = lab_hco.parent AND lab_hco.company = pe.company
-        SET l.department_hsu = lab_hco.service_unit
-        WHERE l.parent IN %s
-    """% tuple(encounters))
+        UPDATE `tabRadiology Procedure Prescription` lrpt
+        INNER JOIN `tabPatient Encounter` pe ON lrpt.parent = pe.name
+        INNER JOIN `tabRadiology Examination Template` template ON lrpt.radiology_examination_template = template.name
+                AND template.disabled = 0
+        INNER JOIN `tabHealthcare Company Option` hco ON template.name = hco.parent AND hco.company = pe.company
+        SET lrpt.department_hsu = hco.service_unit
+        WHERE pe.name IN (%s)
+    """%frappe.db.escape(tuple(encounters)))
 
 
-    # frappe.db.sql("""
-    #     UPDATE `tabLab Prescription` l, `tabRadiology Procedure Prescription` r,
-    #             `tabProcedure Prescription` p, `tabTherapy Plan Detail` t
+    frappe.db.sql("""
+        UPDATE `tabProcedure Prescription` lrpt
+        INNER JOIN `tabPatient Encounter` pe ON lrpt.parent = pe.name
+        INNER JOIN `tabProcedure Prescription` template ON lrpt.procedure = template.name AND template.disabled = 0
+        INNER JOIN `tabHealthcare Company Option` hco ON template.name = hco.parent AND hco.company = pe.company
+        SET lrpt.department_hsu = hco.service_unit
+        WHERE pe.name IN (%s)
+    """%frappe.db.escape(tuple(encounters)))
 
-    #     INNER JOIN `tabPatient Encounter` lab_pe ON l.parent = lab_pe.name 
-    #     INNER JOIN `tabPatient Encounter` rad_pe ON r.parent = rad_pe.name
-    #     INNER JOIN `tabPatient Encounter` proc_pe ON p.parent = proc_pe.name
-    #     INNER JOIN `tabPatient Encounter` tt_pe ON t.parent = tt_pe.name
 
-    #     LEFT JOIN `tabLab Test Template` lab ON l.lab_test_code = lab.name AND lab.disabled = 0
-    #     LEFT JOIN `Healthcare Company Option` lab_hco ON lab.name = lab_hco.parent AND lab_hco.company = pe.company
-
-    #     LEFT JOIN `tabRadiology Examination Template` rad ON r.radiology_examination_template = rad.name
-    #             AND rad.disabled = 0
-    #     LEFT JOIN `Healthcare Company Option` rad_hco ON rad.name = rad_hco.parent AND rad_hco.company = pe.company
-
-    #     LEFT JOIN `tabProcedure Prescription` proc ON p.procedure = proc.name AND proc.disabled = 0
-    #     LEFT JOIN `Healthcare Company Option` proc_hco ON proc.name = proc_hco.parent AND proc_hco.company = pe.company
-
-    #     LEFT JOIN `tabTherapy Type` tt ON t.therapy_type = tt.name AND tt.disabled = 0
-    #     LEFT JOIN `Healthcare Company Option` tt_hco ON tt.name = tt_hco.parent AND tt_hco.company = pe.company
-
-    #     SET l.department_hsu = lab_hco.service_unit, r.department_hsu = rad_hco.service_unit, 
-    #         p.department_hsu = proc_hco.service_unit, t.department_hsu = tt_hco.service_unit
-        
-    #     WHERE pe.name IN %s
-    # """.format(tuple(encounters)))
+    frappe.db.sql("""
+        UPDATE `tabTherapy Plan Detail` lrpt
+        INNER JOIN `tabPatient Encounter` pe ON lrpt.parent = pe.name
+        INNER JOIN `tabTherapy Type` template ON lrpt.therapy_type = template.name AND template.disabled = 0
+        INNER JOIN `tabHealthcare Company Option` hco ON template.name = hco.parent AND hco.company = pe.company
+        SET lrpt.department_hsu = hco.service_unit
+        WHERE pe.name IN (%s)
+    """%frappe.db.escape(tuple(encounters)))
 
     # child_fields = [
     #     {
