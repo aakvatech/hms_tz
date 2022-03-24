52050b56c411618ed05ed71e7c9a60fa4b0e3e89
eliusmgani <eliasmgani@gmail.com>
2022-03-24 17:41:05 +0000

    fix: set query to show main medical department

--- hms_tz/hms_tz/doctype/medical_department/medical_department.js
+++ hms_tz/hms_tz/doctype/medical_department/medical_department.js
@@ -2,4 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Medical Department', {
+    refresh: function(frm) {
+        frm.set_query("main_department", function(){
+            return {
+                filters:[
+                    ["Medical Department", "is_main", "=", 1]
+                ]
+            };
+        });
+    }
 });
