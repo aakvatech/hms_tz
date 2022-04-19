3a582816bb32acca9fe396eb2f62f40c4415e3ed
eliusmgani <eliasmgani@gmail.com>
2022-04-19 06:50:33 +0000

    feat: original items table to allow prescribe medicines which are out of stock so as patient can buy outside pharmacy

--- hms_tz/nhif/api/delivery_note.py
+++ hms_tz/nhif/api/delivery_note.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 from hms_tz.nhif.api.healthcare_utils import update_dimensions
+from frappe.model.naming import set_new_name
+import json
 
 
 def validate(doc, method):
@@ -9,6 +11,7 @@ def validate(doc, method):
         return
     set_prescribed(doc)
     set_missing_values(doc)
+    check_item_for_out_of_stock(doc)
     update_dimensions(doc)
 
 
@@ -21,6 +24,22 @@ def set_original_item(doc):
         if item.item_code:
             item.original_item = item.item_code
             item.original_stock_uom_qty = item.stock_qty
+        
+        new_row = item.as_dict()
+        new_row.update({
+            'name': None,
+            'owner': None,
+            'creation': None,
+            'modified': None,
+            'modified_by': None,
+            'docstatus': None,
+            'dn_detail': item.name,
+            'parent': doc.name,
+            'parentfield': 'hms_tz_original_items',
+            'parenttype': 'Delivery Note',
+            'doctype': 'Original Delivery Note Item'
+        })
+        doc.append('hms_tz_original_items', new_row)
     doc.save(ignore_permissions=True)
 
 
@@ -130,10 +149,101 @@ def update_drug_prescription(doc):
                                 item.delivered_quantity = item.quantity - item.quantity_returned
                                 item.db_update()
 
-# frappe.db.sql("""
-#     UPDATE `tabDrug Prescription` dp
-#     INNER JOIN `tabDelivery Note Item` dni ON dp.name = dni.reference_name
-#     SET dp.quantity = dni.stock_qty, dp.dn_detail = dni.name
-#     WHERE dni.stock_qty != dp.quantity
-#         AND dni.reference_doctype = "Drug Prescription"
-#         AND dni.parent = '{0}'""".format(doc.name))
+def check_item_for_out_of_stock(doc):
+    if len(doc.items) > 0 and len(doc.hms_tz_original_items) > 0:
+        items = []
+
+        for row in doc.items:
+            for item in doc.hms_tz_original_items:
+                if (
+                    row.hms_tz_is_out_of_stock == 1 
+                    and row.name == item.dn_detail
+                    and item.hms_tz_is_out_of_stock == 0
+                ):
+                    item.hms_tz_is_out_of_stock = 1
+                
+                if (
+                    row.hms_tz_is_out_of_stock == 0
+                    and row.name == item.dn_detail
+                    and item.hms_tz_is_out_of_stock == 1
+                ):
+                    item.hms_tz_is_out_of_stock = 0
+            
+            if not row.hms_tz_is_out_of_stock:
+                items.append(row)
+
+        doc.items = items
+        if len(doc.items) == 0:
+            check_out_of_stock_for_original_item(doc)
+            
+
+def check_out_of_stock_for_original_item(doc):
+    for row in doc.hms_tz_original_items:
+        if row.hms_tz_is_out_of_stock == 1:
+            new_row = row.as_dict()
+            new_row.update({
+                'name': row.dn_detail,
+                'owner': None,
+                'creation': None,
+                'modified': None,
+                'modified_by': None,
+                'docstatus': None,
+                'dn_detail': '',
+                'parent': doc.name,
+                'parentfield': 'items',
+                'parenttype': 'Delivery Note',
+                'doctype': 'Delivery Note Item'
+            })
+            doc.append('items', frappe.get_doc(new_row).as_dict())
+    doc.db_update()
+    frappe.msgprint("<h4 class='font-weight-bold bg-warning text-center'>All Items are marked as Out of Stock</h4>")
+
+@frappe.whitelist() 
+def convert_to_instock_item(name, row):
+    """
+    Convert an item to be considered as
+
+    _extended_summary_
+
+    Arguments:
+        name -- _description_
+        row -- _description_
+
+    Returns:
+        _description_
+    """
+    new_row = json.loads(row)
+    og_item_name = new_row['name']
+
+    new_row.update({
+        'name': None,
+        'owner': None,
+        'creation': None,
+        'modified': None,
+        'modified_by': None,
+        'docstatus': None,
+        'dn_detail': None,
+        'hms_tz_is_out_of_stock': 0,
+        'parent': name,
+        'parentfield': 'items',
+        'parenttype': 'Delivery Note',
+        'doctype': 'Delivery Note Item'
+    })
+    doc = frappe.get_doc("Delivery Note", name)
+    prev_size = len(doc.items)
+    doc.append('items', new_row)
+    
+    if len(doc.items) > prev_size:
+        for item in doc.hms_tz_original_items:
+            for entry in doc.items:
+                if (
+                    item.name == og_item_name and
+                    item.item_code == entry.item_code
+                ):
+                    item.dn_detail = entry.name
+                    item.hms_tz_is_out_of_stock = 0
+                    break
+        doc.save(ignore_permissions=True)
+        doc.reload()
+        return True
+
